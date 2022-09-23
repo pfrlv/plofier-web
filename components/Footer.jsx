dbe3cac5f2feeb1f27e5e3bc399bc333b5fd5728
Pasha Frolov <pfrlvgg@gmail.com>
2022-09-23 22:44:59 +0300

    ref: add media metadata & misc improvements

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="editor text-[11px] leading-[15px] text-white/60 sm:text-gray-400 w-[300px] sm:w-auto sm:max-w-[250px] px-[10px]">
+    <div className="editor text-[11px] leading-[15px] text-white sm:text-black w-[300px] sm:w-auto sm:max-w-[250px] px-[10px]">
       Simple web app for background music from the 
       <a href="https://boxradio.net/" rel="noopener noreferrer" target="_blank">
         Box Radio
--- hooks/use-stream-audio.js
+++ hooks/use-stream-audio.js
@@ -12,36 +12,58 @@ function useStreamAudio() {
   const { playing, canplay } = useSnapshot(state)
   const audio = useRef()
 
-  useEffect(() => {
-    audio.current = new Audio(STREAM_URL)
-  }, [])
-
-  useEffect(() => {
-    const onCanPlay = () => {
-      state.canplay = true
-    }
-
-    audio.current.addEventListener('loadedmetadata', onCanPlay)
-
-    return () => {
-      audio.current.pause()
-      audio.current.removeEventListener('loadedmetadata', onCanPlay)
-    }
-  }, [])
-
-  const play = useCallback(() => {
+  const play = useCallback(async () => {
     if (!canplay) return
-
-    audio.current.play()
-    state.playing = true
+    await audio.current.play()
   }, [canplay])
 
   const pause = useCallback(() => {
     if (!canplay) return
-
     audio.current.pause()
-    state.playing = false
   }, [canplay])
+
+  useEffect(() => {
+    audio.current = new Audio(STREAM_URL)
+
+    if ('mediaSession' in navigator) {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'Lo-Fi, Chillhop, ChillJazz, Sleep Music, Work Music etc.',
+        artist: 'LoFi Radio',
+        artwork: [
+          { src: '/mediasession/96.png', sizes: '96x96', type: 'image/png' },
+          { src: '/mediasession/128.png', sizes: '128x128', type: 'image/png' },
+          { src: '/mediasession/192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/mediasession/256.png', sizes: '256x256', type: 'image/png' },
+          { src: '/mediasession/384.png', sizes: '384x384', type: 'image/png' },
+          { src: '/mediasession/512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const onPause = () => (state.playing = false)
+    audio.current.addEventListener('pause', onPause)
+
+    return () => audio.current.removeEventListener('pause', onPause)
+  }, [])
+
+  useEffect(() => {
+    const onPlay = () => (state.playing = true)
+    audio.current.addEventListener('play', onPlay)
+
+    return () => audio.current.removeEventListener('play', onPlay)
+  }, [])
+
+  useEffect(() => {
+    const onCanplay = () => (state.canplay = true)
+    audio.current.addEventListener('loadedmetadata', onCanplay)
+
+    return () => {
+      audio.current.pause()
+      audio.current.removeEventListener('loadedmetadata', onCanplay)
+    }
+  }, [])
 
   return {
     canplay,
