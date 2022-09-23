6f813497c1959eab4bf0deefefd7dafd41f9471e
Pasha Frolov <pfrlvgg@gmail.com>
2022-09-23 14:09:38 +0300

    feat: add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -103,6 +104,21 @@ export default function Document() {
           as="audio"
           href="https://play.streamafrica.net/lofiradio"
         />
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+          ym(90521637, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+          });
+        `}
+        </Script>
       </Head>
       <body className="cursor-default antialiased overflow-hidden">
         <Main />
