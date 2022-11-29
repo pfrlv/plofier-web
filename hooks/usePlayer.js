99f34275a4ecc9975867bd14160e63d489549835
Pasha Frolov <pfrlvgg@gmail.com>
2022-11-29 23:08:13 +0300

    feat: add mediasession metadata

--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -22,6 +22,21 @@ const initPlayer = () => {
     if (state.audio) return
     state.audio = ref(new Audio(STREAM_URL))
 
+    if ('mediaSession' in navigator) {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'Lo-Fi, Chillhop, ChillJazz, Sleep Music, Work Music etc.',
+        artist: 'LoFi Radio',
+        artwork: [
+          { src: '/mediasession/96.png', sizes: '96x96', type: 'image/png' },
+          { src: '/mediasession/128.png', sizes: '128x128', type: 'image/png' },
+          { src: '/mediasession/192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/mediasession/256.png', sizes: '256x256', type: 'image/png' },
+          { src: '/mediasession/384.png', sizes: '384x384', type: 'image/png' },
+          { src: '/mediasession/512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      })
+    }
+
     function onCanplay() {
       state.canplay = true
     }
