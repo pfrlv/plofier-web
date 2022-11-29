e732018bae28c5a2926148a95dce17c1fb6e30c4
Pasha Frolov <pfrlvgg@gmail.com>
2022-11-29 11:34:43 +0300

    chore: add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -99,19 +99,17 @@ export default function Document() {
 
         <link rel="manifest" href="/manifest.webmanifest" />
 
-        <Script id="yandex-metrika" strategy="afterInteractive">
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-          ym(90521637, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true
-          });
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-JYTF9LDLXZ"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-JYTF9LDLXZ');
+          </script>
         `}
         </Script>
       </Head>
