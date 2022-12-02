400e30edb2d4b455b6d6a1d7da2403b54c9d2462
Pasha Frolov <pfrlvgg@gmail.com>
2022-12-02 16:01:02 +0300

    feat: add gihub button

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,14 +1,19 @@
-import { RiPlayMiniFill, RiPauseMiniFill } from 'react-icons/ri'
+import { RiPlayMiniFill, RiPauseMiniFill, RiGithubFill } from 'react-icons/ri'
 
 import usePlayer from '@/hooks/usePlayer'
 import Footer from '@/components/Footer'
 
-const Button = ({ onClick, children, canplay = false }) => {
+const Button = ({ className, children, ...props }) => {
+  let classNames = [
+    'text-white h-[50px] bg-white/5 rounded-[13px] w-[50px] inline-flex justify-center items-center active:scale-95 transition-transform',
+    className
+  ]
+    .filter(Boolean)
+    .join(' ')
+    .trim()
+
   return (
-    <button
-      onClick={onClick}
-      className={`text-white h-[50px] bg-white/5 rounded-[13px] w-[50px] inline-flex justify-center items-center active:scale-95 transition-transform ${!canplay && 'text-white/30 cursor-wait'}`}
-    >
+    <button className={classNames} {...props}>
       {children}
     </button>
   )
@@ -18,7 +23,10 @@ const PlayButton = () => {
   const { playing, canplay, play, pause } = usePlayer()
 
   return (
-    <Button onClick={playing ? pause : play} canplay={canplay}>
+    <Button
+      onClick={playing ? pause : play}
+      className={!canplay && '!text-white/30 !cursor-wait'}
+    >
       {playing ? (
         <RiPauseMiniFill fontSize={40} />
       ) : (
@@ -28,13 +36,30 @@ const PlayButton = () => {
   )
 }
 
+const GithubButton = () => {
+  return (
+    <Button>
+      <a
+        href="https://github.com/pfrlv/plofier-web"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <RiGithubFill fontSize={40} />
+      </a>
+    </Button>
+  )
+}
+
 export default function Player() {
   return (
     <div className="flex flex-wrap justify-center">
-      <div
-        className="select-none bg-black/20 backdrop-blur-md px-[5px] py-[5px] mb-[25px] sm:mb-0 rounded-[17px]">
+      <div className="select-none bg-black/20 backdrop-blur-md px-[5px] py-[5px] mb-[25px] sm:mb-0 rounded-[17px]">
         <div className="flex justify-center items-center">
-          <PlayButton />
+
+          <div className="flex space-x-[5px]">
+            <PlayButton />
+            <GithubButton />
+          </div>
 
           <div className="hidden sm:block">
             <Footer />
