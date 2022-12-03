ce93bbe23043e614d259bda5287880a59d587425
Pasha Frolov <pfrlvgg@gmail.com>
2022-12-03 16:35:04 +0300

    fix: gtag behavior

--- pages/_app.js
+++ pages/_app.js
@@ -15,13 +15,17 @@ function MyApp({ Component, pageProps }) {
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-DGVJ306842"
         strategy="afterInteractive"
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || []
-          function gtag() { window.dataLayer.push(arguments) }
-          gtag('js', new Date())
-          gtag('config', 'G-DGVJ306842')
-        }}
       />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DGVJ306842');
+        `}
+      </Script>
 
       <Component {...pageProps} />
     </>
